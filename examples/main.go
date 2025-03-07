5f68136f16ca421687ced94c4e3dc8eca823dcb1
Crosline <cagkancaglayanel@gmail.com>
2025-03-07 11:54:58 +0300

    example update

--- examples/main.go
+++ examples/main.go
@@ -3,30 +3,30 @@ package examples
 import (
 	"flag"
 
-	"fmt"
-
-	"os"
-	"os/signal"
-	"syscall"
+	"github.com/crosline/gotur"
+	"github.com/crosline/gotur/socket"
 )
 
 func main() {
 	port := flag.Int("port", 8080, "Port to listen on")
 	host := flag.String("host", "0.0.0.0", "Host address to bind to")
 	flag.Parse()
-	
-	cfg := config.New(*host, *port)
 
-	httpServer := server.New(cfg)
-	httpServer.Start()
 
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-	
-	fmt.Printf("Server started on %s:%d\n", cfg.Host(), cfg.Port())
-	
-	// Wait for shutdown signal
-	<-sigChan
-	fmt.Println("Shutting down server...")
-	httpServer.Stop()
+	udp, err := socket.NewUDPSocket()
+	if err != nil {
+		panic(err)
+	}
+
+	if err = udp.Bind(*host, *port); err != nil {
+		panic(err)
+	}
+
+	if err = udp.Listen(); err != nil {
+		panic(err)
+	}
+
+	server = gotur.NewBaseServer(*udp)
+
+	<-make(chan struct{})
 }
